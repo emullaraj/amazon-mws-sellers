2db633cad32dea7d03004bdba23ebb4f9d1be1f2
DEV - Eno Mullaraj <emullaraj.a4b@gmail.com>
2022-07-21 13:10:28 +0200

    [IMP] Remove deprecated function usage

--- src/MarketplaceWebServiceSellers/Client.php
+++ src/MarketplaceWebServiceSellers/Client.php
@@ -217,10 +217,6 @@ class MarketplaceWebServiceSellers_Client implements MarketplaceWebServiceSeller
      */
     public function __construct($awsAccessKeyId, $awsSecretAccessKey, $applicationName, $applicationVersion, $config = null)
     {
-        iconv_set_encoding('output_encoding', 'UTF-8');
-        iconv_set_encoding('input_encoding', 'UTF-8');
-        iconv_set_encoding('internal_encoding', 'UTF-8');
-
         $this->_awsAccessKeyId = $awsAccessKeyId;
         $this->_awsSecretAccessKey = $awsSecretAccessKey;
         if (!is_null($config)) $this->_config = array_merge($this->_config, $config);
